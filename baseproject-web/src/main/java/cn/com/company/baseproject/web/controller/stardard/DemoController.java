9854c2e638bba44c3a174dd7d441fa787a0738f5
Ron <software1010@163.com>
2015-08-01 22:33:33 +0800

    集成maven打包插件

--- baseproject-web/src/main/java/cn/com/company/baseproject/web/controller/stardard/DemoController.java
+++ baseproject-web/src/main/java/cn/com/company/baseproject/web/controller/stardard/DemoController.java
@@ -1,11 +1,28 @@
 package cn.com.company.baseproject.web.controller.stardard;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Demo controller
  * 
  * @author Ron
  * @date 2015年8月1日
  */
+@Controller
+@RequestMapping("/demo")
 public class DemoController {
+
+	/**
+	 * test
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/test")
+	@ResponseBody
+	String test() {
+		return "success";
+	}
 
 }
